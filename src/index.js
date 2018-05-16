9a9d8a057893b3230ac26335eb885288b962f01d
Jeffrey Rios <yefriddavid@gmail.com>
2018-05-16 17:33:10 -0300

    Mejoras

--- src/index.js
+++ src/index.js
@@ -4,6 +4,12 @@
 
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
+//import { fakeAuth } from '../http/fakeAuth'
+import React from 'react'
+//import { connect } from 'react-redux'
+// import PropTypes from 'prop-types'
+// import RefreshTokenPage from '../containers/RefreshTokenPageContainer'
+import { Route, Redirect } from 'react-router-dom';
 
 import styles from './styles.css'
 
@@ -25,20 +31,27 @@ export default class ExampleComponent extends Component {
   }
 }
 
-export class David extends Component {
-  static propTypes = {
-    text: PropTypes.string
-  }
 
-  render() {
-    const {
-      text
-    } = this.props
 
-    return (
-      <div className={styles.test}>
-        Soy David y aca iestoy creando una libreria {text}
-      </div>
-    )
-  }
+const FriendlyRoutes = ({ component: Component, ...rest }) => {
+  let element
+  //if(fakeAuth.isAuthenticated()){
+    element = (<Component {...rest} />)
+  //}else
+  //  element = (<Redirect to={{ pathname: '/oauth/login', state: { from: rest.location }}} />)
+  return (
+    <Route {...rest} render={ (props) => (element)} />
+  )
 }
+
+/*FriendlyRoutes.propTypes = {
+  //accessState: PropTypes.bool.isRequired
+}
+
+const mapStateToProps = state => ({
+  //accessState: state.auth.access.success
+})
+
+export default connect(mapStateToProps)(FriendlyRoutes)*/
+
+

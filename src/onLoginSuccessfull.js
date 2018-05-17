5d256a43fe2d2c4cd5e985875e3ad92999686f14
Jeffrey Rios <yefriddavid@gmail.com>
2018-05-17 16:40:28 -0300

    Mejores

--- src/onLoginSuccessfull.js
+++ src/onLoginSuccessfull.js
@@ -9,6 +9,7 @@ import { Route, Redirect } from 'react-router-dom';
 export default class onLoginSuccessfull extends Component {
 
   render() {
+    console.log(process.env)
     return (
       <div>
         onlogin successfull

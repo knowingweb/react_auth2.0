eb6dc0b5de7436107277dcb49c06ec9604d41eb2
Jeffrey Rios <yefriddavid@gmail.com>
2018-05-17 12:41:44 -0300

    ttt

---
+++ src/onLoginSuccessfull/index.js
@@ -0,0 +1,21 @@
+/**
+ * @class ExampleComponent
+ */
+
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+import { Route, Redirect } from 'react-router-dom';
+
+export default class onLoginSuccessfull extends Component {
+
+  render() {
+    return (
+      <div>
+        onlogin successfull
+      </div>
+    )
+  }
+}
+
+
+

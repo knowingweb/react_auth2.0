1ab7db99ab55b6fc99461f8ec8b1a501e68264e8
Jeffrey Rios <yefriddavid@gmail.com>
2018-05-18 11:36:16 -0300

    Mejoras

--- src/onLoginSuccessfull.js
+++ src/onLoginSuccessfull.js
@@ -3,11 +3,11 @@
  */
 
 import * as appStorage from './libs/appStorage'
-import * as history from '../../history/browserHistory'
-import { APP_PREFIX } from '../../services/config'
+///import * as history from '../../history/browserHistory'
+//import { APP_PREFIX } from '../../services/config'
 
 //eslint-disable-next-line
-export default function* onLoginSuccessfull(response) {
+export default function* onLoginSuccessfull(response, history) {
   appStorage.setSessionInfoData(response.data)
   history.default.push(`/${APP_PREFIX}/app/home`)
 }

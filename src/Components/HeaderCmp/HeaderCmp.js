3b17a8e99342cb32728be58a255460244c0fa5ec
Sergio Sebastian Arriagada <sarriagada87@gmail.com>
2018-05-23 22:09:45 -0300

    update headerCmp

--- src/Components/HeaderCmp/HeaderCmp.js
+++ src/Components/HeaderCmp/HeaderCmp.js
@@ -10,12 +10,13 @@ import styles from './HeaderCmp.css'
 
 export default class HeaderCmp extends Component {
   render() {
+    const { left, center, right } = this.props;
     return (
       <div className={styles.HeaderCmp}>
         <Nav className={styles.HeaderCmp__nav}>
-          <div>{props.left}</div>
-          <div>{props.center}</div>
-          <div>{props.right}</div>
+          <div>{left}</div>
+          <div>{center}</div>
+          <div>{right}</div>
         </Nav>
       </div>
     )

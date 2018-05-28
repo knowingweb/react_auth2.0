5005c6075cd30f8bad8927b5918c8a056acefd9f
Sergio Sebastian Arriagada <sarriagada87@gmail.com>
2018-05-27 23:48:49 -0300

    add collapse component

---
+++ src/Components/Elements/CollapseCmp/CollapseCmp.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react';
+import { Collapse, Button, CardBody, Card } from 'reactstrap';
+
+import styles from './CollapseCmp.css'
+
+import ArrowFoward from '../../../img/arrow-foward';
+
+class CollapseCmp extends Component {
+  constructor(props) {
+    super(props);
+    this.toggle = this.toggle.bind(this);
+    this.state = { collapse: false };
+  }
+
+  toggle() {
+    this.setState({ collapse: !this.state.collapse });
+  }
+
+  render() {
+    const { title, children, className } = this.props;
+    return (
+      <div className={[styles.CollapseCmp, (this.state.collapse ? styles["CollapseCmp--open"] : ""), className].join(" ")}>
+        <Button onClick={this.toggle} className={styles.CollapseCmp__btn}>
+          { title }
+          <ArrowFoward className={styles.CollapseCmp__arrow} ariaLabel='>' />
+        </Button>
+        <Collapse isOpen={this.state.collapse} className={styles.CollapseCmp__content}>
+          { children }
+        </Collapse>
+      </div>
+    );
+  }
+}
+
+export default CollapseCmp;
--- src/Components/HeaderCmp/HeaderCmp.js
+++ src/Components/HeaderCmp/HeaderCmp.js
@@ -10,13 +10,10 @@ import styles from './HeaderCmp.css'
 
 export default class HeaderCmp extends Component {
   render() {
-    const { left, center, right } = this.props;
     return (
-      <div className={styles.HeaderCmp}>
+      <div className={this.props.className}>
         <Nav className={styles.HeaderCmp__nav}>
-          <div>{left}</div>
-          <div>{center}</div>
-          <div>{right}</div>
+          { this.props.children }
         </Nav>
       </div>
     )
